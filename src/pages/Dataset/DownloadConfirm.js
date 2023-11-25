b4f53fb1509cf85f5e79ae4ff6844f20f2942294
NethmiRanasinghe <e18282@eng.pdn.ac.lk>
2023-11-25 10:03:40 +0530

    Fix download - now downloads through the browser

--- src/pages/Dataset/DownloadConfirm.js
+++ src/pages/Dataset/DownloadConfirm.js
@@ -54,6 +54,7 @@ export default function DownloadConfirm({open, setOpen, type}) {
     const [email, setEmail] = React.useState("");
     const [passkey, setPasskey] = React.useState("");
     const [errorMsg, setErrorMsg] = React.useState("");
+    const [successMsg, setSuccessMsg] = React.useState("");
     const [loading, setLoading] = React.useState(false);
 
     const handleClickShowPassword = () => setShowPassword((show) => !show);
@@ -64,7 +65,9 @@ export default function DownloadConfirm({open, setOpen, type}) {
 
     const handleClose = () => {
         setOpen(false);
+        setSuccessMsg("");
         setErrorMsg("");
+        setLoading(false);
         setEmail("");
         setPasskey("");
     };
@@ -75,20 +78,29 @@ export default function DownloadConfirm({open, setOpen, type}) {
         return;
       }
       setLoading(true);
-      axios.post(`${process.env.REACT_APP_BE_URL}/dataset/download`, {
+      axios({
+        method: 'post',
+        url: `${process.env.REACT_APP_BE_URL}/dataset/session`,
+        data: {
           email,
           passkey,
-          type: descriptions[type]?.name
-      },{responseType: 'blob'})
+        },
+      })
+    
       .then((response) => {
-        const blob = new Blob([response.data], { type: response.headers['content-type']});
-        const url = window.URL.createObjectURL(blob);
-        const a = document.createElement('a');
-        a.href = url;
-        a.download = type;
-        a.click();
-        window.URL.revokeObjectURL(url);
-        setOpen(false)
+        const sessionID = response.data.sessionId;
+
+        // trigger download through webbroswer
+        const url = window.URL.createObjectURL(new Blob());
+        const downloadLink = document.createElement('a');
+        downloadLink.href = `${process.env.REACT_APP_BE_URL}/dataset/download?sessionID=${sessionID}&fileName=${descriptions[type].name}`; 
+        downloadLink.download = descriptions[type].name; 
+        document.body.appendChild(downloadLink);
+        downloadLink.click();
+        document.body.removeChild(downloadLink);
+
+        setSuccessMsg("Download started. Please check your browser download bar.");
+        // setLoading(false);
       })
       .catch(function (error) {
           if(error.response?.data?.message){
@@ -96,9 +108,7 @@ export default function DownloadConfirm({open, setOpen, type}) {
           }else{
               alert(error, "error")
           }
-      }).finally(()=>{
-        setLoading(false)
-      })        
+      })       
     };
 
   return (
@@ -108,6 +118,9 @@ export default function DownloadConfirm({open, setOpen, type}) {
         <DialogContent>
           <DialogContentText>
             <span style={{color:"red"}}>{errorMsg}</span>
+          </DialogContentText>
+          <DialogContentText>
+            <span style={{color:"green"}}>{successMsg}</span>
           </DialogContentText>
           <Stack spacing={2} marginY={3}>
           <TextField
@@ -149,7 +162,7 @@ export default function DownloadConfirm({open, setOpen, type}) {
         </DialogContent>
         <DialogActions>
           <LoadingButton loading={loading} variant='contained' color='primary' onClick={handleDownload}>Download</LoadingButton>
-          <Button variant='contained' color='inherit' onClick={handleClose}>Cancel</Button>
+          <Button variant='contained' color='inherit' onClick={handleClose}>Close</Button>
         </DialogActions>
       </Dialog>
     </div>
